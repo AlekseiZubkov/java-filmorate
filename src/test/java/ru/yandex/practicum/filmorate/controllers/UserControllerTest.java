8400cb2a776a5393022b4d5063fbe54c2e4cd0e5
Alex Zubkov <zaleksei95@yandex.ru>
2023-05-23 00:02:40 +0300

    9.2.2

--- src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -5,17 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-
 class UserControllerTest {
 
     User user;
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
 
     }
+
     @Test
     void findAll() {
     }
